49c7ab617219bd61edc856edf76fa3028474e756
Joren Paps <jorenpaps@gmail.com>
2018-07-19 21:32:37 +0200

    it builds!!!

--- keyboards/kc60/keymaps/joren/keymap.c
+++ keyboards/kc60/keymaps/joren/keymap.c
@@ -1,6 +1,5 @@
 
 #include QMK_KEYBOARD_H
-#include "keymap_common.h"
 #include "action_code.h"
 #include "action_layer.h"
 
@@ -19,10 +18,12 @@ enum layer_id {
 // Keycodes
 enum custom_keycodes {
     C_LCKEXT = SAFE_RANGE,
-    C_UNLEXT
-}
+    C_CLSEXT,
+    C_AACUTE,
+    C_AGRAVE
+};
 
-// Fillers to make layering more clear
+// Fillers to make layering clearer
 #define ______ KC_TRNS
 #define XXXXXX KC_NO
 
@@ -75,19 +76,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     * | Ctrl  | Gui   | Alt   |               Space             | AltGr | Left  | Down  | Right |
     * `-----------------------------------------------------------------------------------------'
     */
-  [L_DEFAULT] = LAYOUT(
+  [L_DEFAULT] = LAYOUT_60_ansi(
         KC_ESC,   KC_1,     KC_2,     KC_3,    KC_4,     KC_5,     KC_6,     KC_7,    KC_8,  KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,  \
         KC_TAB,   KC_Q,     KC_W,     KC_E,    KC_R,     KC_T,     KC_Y,     KC_U,    KC_I,  KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_NUBS,  \
-        KC_CAPS,  KC_A,     KC_S,     KC_D,    KC_F,     KC_G,     KC_H,     KC_J,    KC_K,  KC_L,     KC_SCLN,  KC_QUOT,  KC_NO,    KC_ENT,   \
-        KC_LSFT,  KC_NO,    KC_Z,     KC_X,    KC_C,     KC_V,     KC_B,     KC_N,    KC_M,  KC_COMM,  KC_DOT,   KC_SLSH,  KC_NO,    KC_RSFT,  \
-        KC_LCTL,  KC_LGUI,  KC_LALT,                     LT(L_SPACE_FN, KC_SPC),             KC_NO,    KC_RALT,  KC_RGUI,  KC_APP,   KC_RCTL \
+        KC_CAPS,  KC_A,     KC_S,     KC_D,    KC_F,     KC_G,     KC_H,     KC_J,    KC_K,  KC_L,     KC_SCLN,  KC_QUOT,            KC_ENT,   \
+        KC_LSFT,  KC_Z,     KC_X,    KC_C,     KC_V,     KC_B,     KC_N,    KC_M,  KC_COMM,  KC_DOT,   KC_SLSH,                      KC_RSFT,  \
+        KC_LCTL,  KC_LGUI,  KC_LALT,                     LT(L_SPACE_FN, KC_SPC),                       KC_RALT,  KC_RGUI,  KC_APP,   KC_RCTL   \
     ),
-  [L_DEFAULT_ANSI] = LAYOUT(
+  [L_DEFAULT_ANSI] = LAYOUT_60_ansi(
         KC_ESC,   KC_1,     KC_2,     KC_3,    KC_4,     KC_5,     KC_6,     KC_7,    KC_8,  KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,  \
         KC_TAB,   KC_Q,     KC_W,     KC_E,    KC_R,     KC_T,     KC_Y,     KC_U,    KC_I,  KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,  \
-        KC_CAPS,  KC_A,     KC_S,     KC_D,    KC_F,     KC_G,     KC_H,     KC_J,    KC_K,  KC_L,     KC_SCLN,  KC_QUOT,  KC_NO,    KC_ENT,   \
-        KC_LSFT,  KC_NO,    KC_Z,     KC_X,    KC_C,     KC_V,     KC_B,     KC_N,    KC_M,  KC_COMM,  KC_DOT,   KC_SLSH,  KC_NO,    KC_RSFT,  \
-        KC_LCTL,  KC_LGUI,  KC_LALT,                     LT(L_SPACE_FN, KC_SPC),             KC_NO,    KC_RALT,  KC_RGUI,  KC_APP,   KC_RCTL \
+        KC_CAPS,  KC_A,     KC_S,     KC_D,    KC_F,     KC_G,     KC_H,     KC_J,    KC_K,  KC_L,     KC_SCLN,  KC_QUOT,            KC_ENT,   \
+        KC_LSFT,  KC_Z,     KC_X,    KC_C,     KC_V,     KC_B,     KC_N,    KC_M,  KC_COMM,  KC_DOT,   KC_SLSH,                      KC_RSFT,  \
+        KC_LCTL,  KC_LGUI,  KC_LALT,                     LT(L_SPACE_FN, KC_SPC),                       KC_RALT,  KC_RGUI,  KC_APP,   KC_RCTL   \
     ),
 
 	/* Overlay: Game Layer (disable left win and SpaceFN)
@@ -103,12 +104,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |    | NO |    |         Space          |    |    |NpFN|    |
      * `-----------------------------------------------------------'
      */
-  [L_GAME] = LAYOUT(
+  [L_GAME] = LAYOUT_60_ansi(
         ______,   ______,   ______,   ______,   ______,   ______,   ______,   ______,   ______,   ______,   ______,   ______,   ______,   KC_BSPC, \
-        ______,   ______,   ______,   ______,   ______,   ______,   ______,   ______,   ______,   ______,   ______,   ______,   ______,   ______, \
-        ______,   ______,   ______,   ______,   ______,   ______,   ______,   ______,   ______,   ______,   ______,   ______,             ______, \
-        ______,   ______,   ______,   ______,   ______,   ______,   ______,   ______,   ______,   ______,   ______,                       ______, \
-        ______,   XXXXXX,   ______,                       KC_SPC,                                 ______,   ______,   ______,TG(L_NUMPAD),______ \
+        ______,   ______,   ______,   ______,   ______,   ______,   ______,   ______,   ______,   ______,   ______,   ______,   ______,   ______,  \
+        ______,   ______,   ______,   ______,   ______,   ______,   ______,   ______,   ______,   ______,   ______,   ______,             ______,  \
+        ______,   ______,   ______,   ______,   ______,   ______,   ______,   ______,   ______,   ______,   ______,                       ______,  \
+        ______,   XXXXXX,   ______,                       KC_SPC,                                           ______,   ______,TG(L_NUMPAD),______   \
     ),
 
     /* Overlay: SpaceFN Layer
@@ -125,12 +126,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * `-----------------------------------------------------------'
      * Br: Browser (forward/back)
      */
-  [L_SPACE_FN] = LAYOUT(
+  [L_SPACE_FN] = LAYOUT_60_ansi(
         ______,   M_PIPE,   M_AT,     M_HASH,   ______,   ______,   ______,   ______,   ______,   M_LCBR,   M_RCBR,   ______,   ______,   KC_DEL, \
         ______,   ______,   ______,   ______,   ______,   KC_BTN4,  KC_BTN5,  KC_HOME,  KC_UP,    KC_END,   ______,   M_LBRC,   M_RBRC,   M_BSLS, \
         KC_CAPS,  ______,   ______,   ______,   ______,   ______,   KC_PGUP,  KC_LEFT,  KC_DOWN,  KC_RIGHT, ______,   ______,             ______, \
-        ______,   ______,   ______,   KC_SPC,   KC_SPC,   KC_SPC,   KC_PGDN,  M_WRDL,   M_WRDR,   ______,   ______,                    ______, \
-        ______,   ______,   ______,                       ______,                                 ______,   ______,   ______,   ______,   ______ \
+        ______,   ______,   ______,   KC_SPC,   KC_SPC,   KC_SPC,   KC_PGDN,  M_WRDL,   M_WRDR,   ______,   ______,                       ______, \
+        ______,   ______,   ______,                       ______,                                           ______,   ______,   ______,   ______ \
     ),
 
     /* Overlay: Extended Function Layer
@@ -150,19 +151,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * Clc: Calculator
      * Tm: Task Manager
      */
-  [L_EXTENDED] = LAYOUT(
+  [L_EXTENDED] = LAYOUT_60_ansi(
         KC_GRV,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_DEL, \
-        C_LCKEXT, KC_BTN4,  KC_WH_U,  KC_BTN5,   ______,   M_TM,     KC_CALC,  KC_HOME,  KC_UP,    KC_END,   ______,   KC_PSCR,  KC_PAUSE, KC_INS, \
-        ______,   KC_WH_L,  KC_WH_D,  KC_WH_R,   ______,   ______,   KC_PGUP,  KC_LEFT,  KC_DOWN,  KC_RIGHT, ______,   ______,             ______, \
+        C_LCKEXT, KC_BTN4,  KC_WH_U,  KC_BTN5,  ______,   M_TM,     KC_CALC,  KC_HOME,  KC_UP,    KC_END,   ______,   KC_PSCR,  KC_PAUSE, KC_INS, \
+        ______,   KC_WH_L,  KC_WH_D,  KC_WH_R,  ______,   ______,   KC_PGUP,  KC_LEFT,  KC_DOWN,  KC_RIGHT, ______,   ______,             ______, \
         ______,   ______,   ______,   ______,   ______,   ______,   KC_PGDN,  ______,   KC_VOLD,  KC_VOLU,  KC_MUTE,                      ______, \
-        ______,   ______,   ______,                       ______,                                 ______,   ______,   ______,TG(L_NUMPAD),______ \
+        ______,   ______,   ______,                       ______,                                           ______,   ______,TG(L_NUMPAD),______  \
   ),
-    // KEYMAP_ANSI(
-    //     GRV ,  F1,  F2,  F3,  F4,  F5,  F6,  F7,  F8,  F9, F10, F11, F12, DEL, \
-    //     FN8 ,BTN4,WH_U,BTN5,______,   FN23,CALC,HOME,  UP, END,______,   PSCR,PAUS, INS, \
-    //     ______,   WH_L,WH_D,WH_R,______,   ______,   PGUP,LEFT,DOWN,RGHT,QUOT,BSLS,      FN7, \
-    //     ______,   ______,   ______,   ______,   ______,   ______,   PGDN,______,   VOLD,VOLU,MUTE,          ______,    \
-    //     ______,   ______,   ______,             SPC,                     ______,    FN3, FN4,______),
 
     /* Overlay: Extended Locking Layer
      * ,-----------------------------------------------------------.
@@ -177,19 +172,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |    |    |    |                        |    |Gui |App |    |
      * `-----------------------------------------------------------'
      */
-  [L_EXTENDED_LOCK] = LAYOUT(
-        KC_ESC,   ______,   ______,   ______,   ______,   ______,   ______,   ______,   ______,   ______,   ______,   ______,   ______,   ______,    \
-        KC_TAB,   ______,   ______,   ______,   ______,   ______,   ______,   ______,   ______,   ______,   ______,   ______,   ______,   ______,    \
-        C_UNLEXT, ______,   ______,   ______,   ______,   ______,   ______,   ______,   ______,   ______,   ______,   ______,             KC_ENT, \
-        ______,   ______,   ______,   ______,   ______,   ______,   ______,   ______,   ______,   ______,   ______,                       ______,    \
-        ______,   ______,   ______,                        ______,                                ______,   ______,   KC_RGUI,  KC_APP,   ______ \
+  [L_EXTENDED_LOCK] = LAYOUT_60_ansi(
+        KC_ESC,   ______,   ______,   ______,   ______,   ______,   ______,   ______,   ______,   ______,   ______,   ______,   ______,   ______, \
+        KC_TAB,   ______,   ______,   ______,   ______,   ______,   ______,   ______,   ______,   ______,   ______,   ______,   ______,   ______, \
+        C_CLSEXT, ______,   ______,   ______,   ______,   ______,   ______,   ______,   ______,   ______,   ______,   ______,             KC_ENT, \
+        ______,   ______,   ______,   ______,   ______,   ______,   ______,   ______,   ______,   ______,   ______,                       ______, \
+        ______,   ______,   ______,                        ______,                                          ______,   KC_RGUI,  KC_APP,   ______  \
   ),
-    // KEYMAP_ANSI(
-    //     ESC ,______,   ______,   ______,   ______,   ______,   ______,   ______,   ______,   ______,   ______,   ______,   ______,   ______,    \
-    //     TAB ,______,   ______,   ______,   ______,   ______,   ______,   ______,   ______,   ______,   ______,   ______,   ______,   ______,    \
-    //     FN9 ,______,   ______,   ______,   ______,   ______,   ______,   ______,   ______,   ______,   ______,   ______,         ENT, \
-    //     ______,   ______,   ______,   ______,   ______,   ______,   ______,   ______,   ______,   ______,   ______,             ______,    \
-    //     ______,   ______,   ______,             ______,                       ______,   RGUI, APP,______),
 
     /* Overlay: Numpad
      * ,-----------------------------------------------------------.
@@ -207,19 +196,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * Mw: Mouse Wheel
      * Mc: Mouse Cursor
      */
-  [L_NUMPAD] = LAYOUT(
+  [L_NUMPAD] = LAYOUT_60_ansi(
         ______,   KC_P1,    KC_P2,    KC_P3,    KC_P4,    KC_P5,    KC_P6,    KC_P7,    KC_P8,    KC_P9,    KC_P0,    KC_PMNS,  KC_PPLS,  KC_BSPC, \
         ______,   ______,   ______,   ______,   ______,   ______,   KC_PMNS,  KC_P7,    KC_P8,    KC_P9,    KC_PSLS,  ______,   ______,   ______, \
         ______,   ______,   ______,   ______,   ______,   ______,   KC_PPLS,  KC_P4,    KC_P5,    KC_P6,    KC_PAST,  ______,             KC_PENT, \
         ______,   ______,   ______,   ______,   ______,   ______,   KC_PDOT,  KC_P1,    KC_P2,    KC_P3,    ______,                       KC_PENT, \
-        ______,   ______,   ______,                       KC_P0,                                  KC_PDOT,  ______,   ______,   ______,   KC_NLCK \
+        ______,   ______,   ______,                       KC_P0,                                            KC_PDOT,  ______,   ______,   KC_NLCK \
   ),
-    // KEYMAP_ANSI(
-    //     ______,   KP_1,KP_2,KP_3,KP_4,KP_5,KP_6,KP_7,KP_8,KP_9,KP_0,PMNS,PPLS,BSPC, \
-    //     ______,   ______,   ______,   ______,   ______,   ______,   PMNS,KP_7,KP_8,KP_9,PSLS,______,   ______,   ______,    \
-    //     ______,   ______,   ______,   ______,   ______,   ______,   PPLS,KP_4,KP_5,KP_6,PAST,______,        PENT, \
-    //     ______,   ______,   ______,   ______,   ______,   ______,   PDOT,KP_1,KP_2,KP_3,______,             PENT, \
-    //     ______,   ______,   ______,             KP_0,                    PDOT,______,   NLCK, FN4),
 
     /* Overlay: Layout selection layer
      * ,-----------------------------------------------------------.
@@ -235,12 +218,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * `-----------------------------------------------------------'
      * Gm: Game
      */
-  [L_CONFIG] = LAYOUT(
+  [L_CONFIG] = LAYOUT_60_ansi(
         ______,   ______,   ______,   ______,   ______,   ______,   ______,   ______,   ______,   ______,   ______,   ______,   ______,   ______, \
         ______,   ______,   ______,   ______,   ______,   ______,   ______,   ______,   ______,   ______,   ______,   ______,   ______,   ______, \
         ______,   ______,   ______,   ______,   ______,   ______,   ______,   ______,   ______,   ______,   ______,   ______,             ______, \
         ______,   ______,   ______,   ______,   ______,   ______,   ______,   ______,   ______,   ______,   ______,                       ______, \
-        ______,   ______,   ______,                       ______,                                 ______,   ______,   ______,   ______,   ______ \
+        ______,   ______,   ______,                       ______,                                           ______,   ______,   ______,   ______ \
   )
 };
 
@@ -255,9 +238,8 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         layer_on(L_EXTENDED_LOCK);
       }
       return false;
-    case C_UNLEXT:
-      // Play a tone when enter is pressed
-      if (record->event.release) {
+    case C_CLSEXT:
+      if (record->event.pressed) {
         layer_off(L_EXTENDED_LOCK);
         layer_off(L_EXTENDED);
       }

4ca36e55519f5fe338143565f4bc6c530e71ea4b
bosjaH <jorenpaps@gmail.com>
2026-02-13 23:41:52 +0100

    Add PandaKB sofle

---
+++ keyboards/sofle/pandakb/config.h
@@ -0,0 +1,38 @@
+#pragma once
+
+// USB detection
+#define USB_VBUS_PIN 19
+
+// Split handedness
+#define	SPLIT_HAND_PIN_LOW_IS_LEFT
+#define SPLIT_HAND_PIN GP4
+
+// Serial communication - half-duplex on PIO0
+#define USE_SERIAL
+#define SERIAL_PIO_USE_PIO0
+#define SOFT_SERIAL_PIN GP1
+// #define SERIAL_USART_TX_PIN GP1
+//#undef SOFT_SERIAL_PIN
+
+
+#ifdef OLED_ENABLE
+    #define I2C_DRIVER I2CD1
+    #define I2C1_SDA_PIN GP2
+    #define I2C1_SCL_PIN GP3
+
+    #define OLED_DISPLAY_128X32
+    #define OLED_BRIGHTNESS 180
+    #define OLED_TIMEOUT 10000
+
+    // #define WPM_ESTIMATED_WORD_SIZE 5
+    #define SPLIT_LAYER_STATE_ENABLE
+    #define SPLIT_LED_STATE_ENABLE
+    #define SPLIT_MODS_ENABLE
+    #define SPLIT_WPM_ENABLE
+    #define SPLIT_OLED_ENABLE
+#endif
+
+#ifdef RGB_MATRIX_ENABLE
+    #define WS2812_DI_PIN GP0
+    #define WS2812_PIO_USE_PIO1
+#endif
---
+++ keyboards/sofle/pandakb/rules.mk
@@ -0,0 +1,17 @@
+# PandaKB Sofle RP2040 settings
+# MCU = RP2040
+# BOOTLOADER = rp2040
+
+# Common features (adjust as needed)
+# SPLIT_KEYBOARD = yes
+# SERIAL_DRIVER = vendor
+
+# RGB_MATRIX_ENABLE = yes
+# RGB_MATRIX_DRIVER = ws2812
+WS2812_DRIVER = vendor
+
+ENCODER_MAP_ENABLE = yes
+
+# OLED_ENABLE = yes
+OLED_DRIVER = ssd1306
+WPM_ENABLE = yes
